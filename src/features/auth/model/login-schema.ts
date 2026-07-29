1fc80a505aa5a2fa84cef94938e7745b49e556fb
추창만 <chuchangman@gmail.com>
2026-07-29 23:05:09 +0900

    feat: 인증 폼 입력 검증 추가

--- src/features/auth/model/login-schema.ts
+++ src/features/auth/model/login-schema.ts
@@ -1,8 +1,12 @@
 import { z } from "zod";
 
 export const loginSchema = z.object({
-  email: z.email("이메일"),
-  password: z.string("비밀번호").nonempty("비었음"),
+  email: z
+    .string()
+    .trim()
+    .nonempty({ error: "이메일을 입력해주세요." })
+    .pipe(z.email({ error: "올바른 이메일 형식으로 입력해주세요." })),
+  password: z.string().nonempty({ error: "비밀번호를 입력해주세요." }),
 });
 
 export type Login = z.infer<typeof loginSchema>;
--- src/features/auth/model/signup-schema.ts
+++ src/features/auth/model/signup-schema.ts
@@ -1,10 +1,28 @@
 import { z } from "zod";
 
-export const signupSchema = z.object({
-  nickName: z.string(),
-  email: z.string(),
-  password: z.string(),
-  correctPassword: z.string(),
-});
+export const signupSchema = z
+  .object({
+    nickName: z.string().trim().nonempty({
+      error: "닉네임을 입력해주세요.",
+    }),
+
+    email: z
+      .string()
+      .trim()
+      .nonempty({ error: "이메일을 입력해주세요." })
+      .pipe(z.email({ error: "올바른 이메일 형식으로 입력해주세요." })),
+
+    password: z.string().nonempty({
+      error: "비밀번호를 입력해주세요.",
+    }),
+
+    correctPassword: z.string().nonempty({
+      error: "비밀번호를 다시 입력해주세요.",
+    }),
+  })
+  .refine((data) => data.password === data.correctPassword, {
+    error: "비밀번호가 일치하지 않아요.",
+    path: ["correctPassword"],
+  });
 
 export type Signup = z.infer<typeof signupSchema>;
